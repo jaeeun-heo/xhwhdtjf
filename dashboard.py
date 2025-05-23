c871fa3aada79959a5a5d7dad4af621dbaa7fabe
허재은 <yellowhorse2@naver.com>
2025-05-23 11:53:25 +0900

    .

--- dashboard.py
+++ dashboard.py
@@ -26,8 +26,9 @@ st.markdown("스마트폰에서 수집한 데이터를 기반으로 이상 탐�
 # 데이터 디렉토리 설정
 
 data_dir = os.path.join("data", "demo_add")
-file_list = glob.glob(os.path.join(data_dir, "demo_*_add.csv"))  # data_1_add.csv, data_2_add.csv, ...
-print(file_list)
+pattern = os.path.join(data_dir, "demo_*_add.csv")
+
+file_list = glob.glob(pattern)
 
 st.write(f"데이터 폴더 경로: {data_dir}")
 st.write(f"찾은 파일 개수: {len(file_list)}")
