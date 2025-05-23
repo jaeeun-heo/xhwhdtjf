8966ecbf9c1f505a1337f62dacac89a5a17e24a2
허재은 <yellowhorse2@naver.com>
2025-05-23 18:44:53 +0900

    .

--- pitch.py
+++ pitch.py
@@ -26,38 +26,39 @@ def show_pitch():
                 df[['position', 'cumulative_pitch', 'tilt', 'position_bin', 'file']]
             ], ignore_index=True)
 
-    # 2. 분석 범위 필터링
     combined_df = combined_df[(combined_df['position'] >= 0) & (combined_df['position'] <= 2.5)]
 
-    # 3. Plotly 그래프 생성
-    fig = go.Figure()
-
-    # 대표 하나의 평균곡선만 그리되, tilt로 굵기 표현
+    # 2. 평균 구하기
     mean_df = combined_df.groupby('position_bin').agg({
         'cumulative_pitch': 'mean',
         'tilt': 'mean'
     }).reset_index()
 
-    # 각 점을 선분으로 연결하면서 굵기 표현
+    # 3. Plotly 선분별로 그리기 (tilt를 굵기로)
+    fig = go.Figure()
+
     for i in range(len(mean_df) - 1):
+        tilt_val = mean_df.loc[i, 'tilt']
+        line_width = max(1, min(12, tilt_val * 40))  # 적절한 스케일 조절
+
         fig.add_trace(go.Scatter(
             x=[mean_df.loc[i, 'position_bin'], mean_df.loc[i + 1, 'position_bin']],
             y=[mean_df.loc[i, 'cumulative_pitch'], mean_df.loc[i + 1, 'cumulative_pitch']],
             mode='lines',
             line=dict(
                 color='royalblue',
-                width=max(1, min(10, mean_df.loc[i, 'tilt'] * 10))  # tilt 기준 굵기 제한 (1~10)
+                width=line_width
             ),
             showlegend=False
         ))
 
-    # 범례용 평균 선 (보이게만)
+    # 참고용 평균선 (점선, 일정 굵기)
     fig.add_trace(go.Scatter(
         x=mean_df['position_bin'],
         y=mean_df['cumulative_pitch'],
         mode='lines',
-        name='Mean Cumulative Pitch (Width = Tilt)',
-        line=dict(color='royalblue', width=3, dash='dot')
+        name='Mean Cumulative Pitch (Reference)',
+        line=dict(color='lightgray', width=2, dash='dot')
     ))
 
     fig.update_layout(
