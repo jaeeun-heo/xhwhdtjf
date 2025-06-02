73654bcaf300bf82d14045438d682135c0348d5f
jaeeun-heo <yellowhorse2@naver.com>
2025-06-02 17:19:39 +0900

    .

--- dashboard.py
+++ dashboard.py
@@ -42,7 +42,7 @@ with button_col:
 
 
 # --- 분석 탭 버튼 ---
-analysis_option = st.radio("분석 항목 선택", ["Gyro", "Pitch"], horizontal=True)
+analysis_option = st.radio("✅ 분석 항목 선택", ["Gyro", "Pitch"], horizontal=True)
 
 
 # --------------------------
